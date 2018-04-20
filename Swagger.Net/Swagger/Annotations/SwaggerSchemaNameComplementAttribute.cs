fbc357f6a27ef5afe3cceef7492657cf9b443309
FMT <fmt@ecoburotic.com>
2018-04-20 14:25:39 +0200

    SwaggerSchemaNameComplement
    (for differentiate 2 objets with same name but in different namespace)

---
+++ Swagger.Net/Swagger/Annotations/SwaggerSchemaNameComplementAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Swagger.Net.Annotations
+{
+    /// <summary>
+    /// Permet de personnaliser la fin du nom d'un schema
+    /// </summary>
+    /// <example>
+    /// In your SwaggerConfig
+    /// 
+    /// c.SchemaId(t =>
+    /// {
+    ///     string name = t.Name;
+    ///
+    ///     foreach (SwaggerNameComplementAttribute swaggerNameComplementAttribute in t.GetCustomAttributes(typeof(SwaggerNameComplementAttribute), false))
+    ///     {
+    ///         name += " " + swaggerNameComplementAttribute.NameComplement;
+    ///     }
+    ///
+    ///     return name;
+    /// });
+    /// </example>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class SwaggerSchemaNameComplementAttribute : Attribute
+    {
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="complement"></param>
+        public SwaggerSchemaNameComplementAttribute(string complement)
+        {
+            this.NameComplement = complement;
+        }
+
+        /// <summary>
+        /// Complement pour le nom de l'objet
+        /// </summary>
+        public string NameComplement { get; private set; }
+    }
+}
